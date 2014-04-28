74d285ac3dad7f0deff0679012291c5373b5e00c
PabloParedes <paparedes@espe.edu.ec>
2014-04-27 22:08:51 -0500

    Inverted matrix
    The program is able to inverte a matrix by taking it from cache

--- cachematrix.R
+++ cachematrix.R
@@ -4,19 +4,20 @@
 ## here is a function that makes a "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-     i  <- NULL
-    set  <- function(y){
-      x <<- y
-      i <<- NULL 
-    }
-    get  <- function() x
-    setinverse  <- function(inverse) i  <<- inverse
-    getinverse  <- function() i
-    list(set= set, get = get, 
-         setinverse = setinverse, 
-         getinverse = getinverse)
-    
+  
+  i  <- NULL
+  set  <- function(y){
+    x <<- y
+    i <<- NULL 
   }
+  get  <- function() x
+  setinverse  <- function(inverse) i  <<- inverse
+  getinverse  <- function() i
+  list(set= set, get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
+  
+}
 }
 
 
